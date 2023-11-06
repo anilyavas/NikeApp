514cea65e69f0eeab7ffefe77741c796d42d3244
Anıl <anil@Anl-MacBook-Air.local>
2023-11-06 12:23:24 +0300

    navigation completed

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,24 +1,28 @@
-import { FlatList, View, Image, Text, StyleSheet } from 'react-native';
+import { FlatList, Pressable, Image, Text, StyleSheet } from 'react-native';
 import products from '../data/products';
 
-export function ProductScreen() {
+export const ProductScreen = ({ navigation }) => {
+  // const navigation = useNavigation();
   return (
     <FlatList
       numColumns={2}
       data={products}
       renderItem={({ item }) => (
-        <View style={styles.itemContainer}>
+        <Pressable
+          style={styles.itemContainer}
+          onPress={() => navigation.navigate('Product Details')}
+        >
           <Image
             style={styles.image}
             source={{
               uri: item.image,
             }}
           />
-        </View>
+        </Pressable>
       )}
     />
   );
-}
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
